123e132781d45776a3ecf704cca1ba634e5b821b
shr34.sachin <shr34.sachin@gmail.com>
2024-06-16 19:42:54 +0545

    commit

---
+++ day1_1.py
@@ -0,0 +1,52 @@
+# # We will start with the variables and different datatypes
+# ''' print(2+2)
+# print(2-3)
+# print(2*3)
+# print(2/3)
+# print(13%2)
+# print(2**3) '''
+
+# print("hellow world")
+# print('hellow world')
+# print("hellow 'w'orld")
+
+# print('hello world1')
+# print('hello world2')
+# print('hello world3')
+# print('hello world4')
+
+# x= 10
+# y = 10.2
+# z = x+y
+# a = 2+3j
+
+# print(type(x))
+# print(type(y))
+# print(type(z))
+# print(type(a))
+# print(x)
+
+# import math
+# print(abs(a))
+# print(math.sqrt(50))
+# print(math.sin(math.radians(30)))
+# # math.
+
+# # String suru bhayo
+# a = 'this is a string'
+# print(a)
+
+# b = a
+# print(b[:5])
+# print(a+' ' + b)
+
+a = [1,2,3,4,5]
+b = ['a','b','c','d']
+c = ['gapple','danana','cat']
+d = ['apple','dog',1782,1154]
+
+c[1] = 'apple'
+print(c)
+
+a.append(6)
+
