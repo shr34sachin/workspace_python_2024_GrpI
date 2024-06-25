e87e342aaeda4ce47fd70313c53f834b50290fff
shr34.sachin <shr34.sachin@gmail.com>
2024-06-25 17:10:07 +0545

    day5 done

---
+++ Day5_GUI_loginform_loop.py
@@ -0,0 +1,56 @@
+# simple login form
+import tkinter as tk
+from tkinter import messagebox
+
+win = tk.Tk()
+win.title('LOGIN FORM')
+
+un_list = ('deepika','binod','bhatta')
+pw_list = ('12345','promax','khatraa')
+
+def login():
+    un = textbox_UN.get()
+    pw = textbox_PASS.get()
+    
+    if un_list.count(un) == 1:
+        indexUN = un_list.index(un)
+        if pw_list[indexUN] == pw:
+            messagebox.showinfo('Login Success', f'Welcome {un}!')
+        else:
+            messagebox.showerror('Login Failed', 'Invalid Username or Password')
+    else:
+        messagebox.showerror('Login Failed', 'Invalid Username or Password')
+    
+    # if un == 'admin' and pw == '12345':
+    #     messagebox.showinfo('Login Success', 'Welcome Prabhat!')
+    # else:
+    #     messagebox.showerror('Login Failed', 'Invalid Username or Password')
+
+label_UN = tk.Label(win,text='USERNAME')
+label_UN.grid(row=0, column=0, padx=20, pady=10)
+textbox_UN =tk.Entry(win)
+textbox_UN.grid(row=0, column=1, padx=20, pady=10)
+label_PASS = tk.Label(win,text='PASSWORD')
+label_PASS.grid(row=1, column=0, padx=20, pady=10)
+textbox_PASS =tk.Entry(win, show='*')
+textbox_PASS.grid(row=1, column=1, padx=20, pady=10)
+btnLogin = tk.Button(win, text='Login', command=login)
+btnLogin.grid(row=2, column=1, padx=20, pady=10)
+
+def change_color():
+    colorVal = radio_var.get()
+    if colorVal == 1:
+        win.config(bg='blue')
+    if colorVal ==2:
+        win.config(bg='red')
+        
+radio_var = tk.IntVar()
+radioBtn = tk.Radiobutton(win, text='blue', 
+                          variable = radio_var, value =1,
+                          command=change_color)
+radioBtn.grid(row=3, column=1, padx=20, pady =10)
+radioBtn = tk.Radiobutton(win, text='red', 
+                          variable = radio_var, value =2,
+                          command=change_color)
+radioBtn.grid(row=4, column=1, padx=20, pady =10)
+win.mainloop()
--- Day5_loop.py
+++ Day5_loop.py
@@ -13,9 +13,50 @@
 #         continue
 #     print(i)
     
-i = 0
-while i<10:
-    i += 1
-    print(i)
-else:
-    print('sakio sakio')
+# i = 0
+# while i<10:
+#     i += 1
+#     print(i)
+# else:
+#     print('sakio sakio')
+
+# # sum odd values, even values and all
+# no = int(input('Enter a number: '))
+# no1, sumEven, sumOdd, sumAll = 0, 0, 0, 0
+# while no1 <= no:
+#     if no1 % 2 == 0:
+#         sumEven += no1
+#     else:
+#         sumOdd += no1
+#     sumAll += no1
+#     no1 += 1
+# print(f'Sum All is {sumAll}; Sum Odd is {sumOdd}; Sum Even is {sumEven}')
+
+# # Fibonacci sequence
+# n = int(input('Enter the count for fibonacci sequence: '))
+# count = 0
+# n1, n2 = 0, 1
+# print(f'Fibonacci up to {n} numbers:')
+# while count < n:
+#     print(n1)
+#     n3 = n1 + n2
+#     n1 = n2
+#     n2 = n3    
+#     count += 1
+    
+# # FOR Loop
+# fruits = ['mango', 'litchi', 'apple', 'orange']
+# for fruit in fruits:
+#     if fruit == 'apple':
+#         continue
+#     print(fruit)
+# else:
+#     print('sakio sakio')
+
+# for i in range(1,10,2):
+#     print(i)
+    
+# multiplication table
+no = int(input('Enter a number: '))
+for i in range(1,11):
+    print(f'{no} x {i} = {no*i}')
